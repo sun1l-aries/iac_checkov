c8aa8fc420340d3a9af2711e7c3cd136053a04a4
sun1l-aries <sri5aries@googlemail.com>
2022-04-12 17:54:11 +0100

    Added two files in the root directory.

---
+++ s3_bucket_tf-template.tf
@@ -0,0 +1,19 @@
+resource "aws_s3_bucket" "S3Bucket" {
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "aws:kms"
+        kms_master_key_id = "aws/s3"
+      }
+    }
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "blockPublicAccess" {
+  bucket = aws_s3_bucket.S3Bucket.id
+  block_public_acls = true
+  block_public_policy = true
+  ignore_public_acls = true
+  restrict_public_buckets = true
+}
