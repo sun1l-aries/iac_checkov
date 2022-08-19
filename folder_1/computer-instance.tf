ba2dee69aa67a829642492165109e3fa5ebadc3c
Sunil Iyengar <sri5aries@goolemail.com>
2022-08-19 14:52:06 +0100

    Trying GCP policy

---
+++ folder_1/computer-instance.tf
@@ -0,0 +1,29 @@
+resource "google_compute_instance" "default" {
+  name         = "virtual-machine-from-terraform"
+  machine_type = "f1-micro"
+  zone         = "us-central1-c"
+
+  tags = ["foo", "bar"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral public IP
+    }
+  }
+
+  metadata = {
+    foo = "bar"
+    block-project-ssh-keys = true
+  }
+
+  metadata_startup_script = "sudo apt-get update && sudo apt-get install apache2 -y && echo '<!doctype html><html><body><h1>Avenue Code is the leading software consulting agency focused on delivering end-to-end development solutions for digital transformation across every vertical. We pride ourselves on our technical acumen, our collaborative problem-solving ability, and the warm professionalism of our teams.!</h1></body></html>' | sudo tee /var/www/html/index.html"
+
+}
