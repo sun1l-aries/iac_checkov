a76c2535724341eca88511e3691ca7eb1b91952d
sun1l-aries <sunil_iyengar@hotmail.com>
2023-02-14 12:44:18 +0000

    testing checkov works in github actions

--- iac_input_files/computer-instance.tf
+++ iac_input_files/computer-instance.tf
@@ -1,7 +1,7 @@
 resource "google_compute_instance" "default" {
   name         = "virtual-machine-from-terraform"
   machine_type = "f1-highmem"
-  zone         = "us-central1-c"
+  zone         = "us-central1-a"
 
   tags = ["foo", "bar"]
 
